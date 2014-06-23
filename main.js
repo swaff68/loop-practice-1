63ed9aef6f13dd61211fc3a27548ba5cadcf0d84
Swaff <swaff68@gmail.com>
2014-06-23 12:58:12 -0600

    loops

---
+++ main.js
@@ -0,0 +1,54 @@
+for (var i =0; i< 5; i ++) {
+	console.log (i)
+}
+
+for (var i =0; i< 501; i+=100) {
+	console.log (i)
+}
+
+for (var i =1; i<65; i*=2) {
+	console.log (i)
+}
+
+for (var i =1; i<4; i++) {
+	console.log (i)
+	console.log (i)
+	console.log (i)
+}
+
+for (var i =0; i<11; i+=2) {
+	console.log(i)
+}
+
+for (var i =3; i<16; i+=3) {
+	console.log(i)
+}
+
+for (var i =9; i>=0; i--) {
+	console.log(i)
+}
+
+for (var i =1; i<4; i++) {
+	for (var z =0; z<4; z++){
+		console.log (z)
+	}
+	
+}
+
+for (var i =1; i<4; i++) {
+	for (var z =0; z<3; z++){
+		console.log (i);
+	}
+}
+
+
+for (var i =2; i<5; i++){
+	for (var z =2; z<9; z+=2)
+		console.log (z)
+}
+
+for (var i =0; i<5; i++){
+	for (var z=0; z<3; z++){
+		console.log(i)
+	}
+}
